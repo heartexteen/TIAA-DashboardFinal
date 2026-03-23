beaebedde2c01026ae1819f9997dd4b7ac922d12
heartexteen <kittimatechulajata@gmail.com>
2026-03-23 11:08:04 -0400

    FixAginNextConfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  basePath: "/TIAA-DashboardFinal",
-  assetPrefix: "/TIAA-DashboardFinal",
-  images: {
-    unoptimized: true,
-  },
   typescript: {
     ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
   },
 };
 
